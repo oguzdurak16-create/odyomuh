ae76ed967d250b229ec4ada7967a2c99316ffce2
oguzdurak16-create <oguzdurak16@gmail.com>
2026-07-20 11:38:46 +0300

    Include daily English stories in RSS

--- app/(en)/en/feed.xml/route.js
+++ app/(en)/en/feed.xml/route.js
@@ -1,11 +1,24 @@
 import { englishPosts } from '../../../../data/en-posts';
+import { dailyEnglishPosts } from '../../../../data/daily-2026-07-20';
 import { baseUrl } from '../../../site-data';
 
 const siteUrl = baseUrl || 'https://www.odyomuh.net';
 const escapeXml = (value = '') => String(value).replace(/[<>&'\"]/g, (char) => ({ '<': '&lt;', '>': '&gt;', '&': '&amp;', "'": '&apos;', '"': '&quot;' }[char]));
 
+function combinedPosts() {
+  const seen = new Set();
+  return [...dailyEnglishPosts, ...englishPosts]
+    .filter((post) => {
+      const key = post.id || post.primaryPath;
+      if (!key || seen.has(key)) return false;
+      seen.add(key);
+      return true;
+    })
+    .sort((a, b) => (b.published || '').localeCompare(a.published || ''));
+}
+
 export async function GET() {
-  const items = englishPosts.slice(0, 30).map((post) => `
+  const items = combinedPosts().slice(0, 30).map((post) => `
     <item>
       <title>${escapeXml(post.title)}</title>
       <link>${siteUrl}${post.primaryPath}</link>
