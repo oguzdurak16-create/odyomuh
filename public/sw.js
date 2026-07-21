17d9de9c1b5070bef4bc6775480778dee3ec2099
oguzdurak16-create <oguzdurak16@gmail.com>
2026-07-21 15:34:11 +0300

    Add lightweight offline service worker

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const CACHE='odyomuh-shell-v1';
+const SHELL=['/','/arsiv','/etiketler','/p/tarih-kronolojisi.html'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response;}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('/'))));});
