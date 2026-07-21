5edf3b8a0a0cce0adb5e7fc97ab9d37d793bc2cf
oguzdurak16-create <oguzdurak16@gmail.com>
2026-07-21 17:00:01 +0300

    Load final interface consistency layer

--- app/(tr)/layout.jsx
+++ app/(tr)/layout.jsx
@@ -5,6 +5,7 @@ import '../experience.css';
 import '../civilization.css';
 import '../platform.css';
 import '../future-lab.css';
+import '../ui-consistency.css';
 import { site, pages, baseUrl, generatedArt } from '../site-data';
 import { allTurkishLabels, allTurkishPosts } from '../../lib/content-collections';
 import SiteChrome from '../../components/SiteChrome';
