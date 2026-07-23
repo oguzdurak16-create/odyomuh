4d96f0e45f69c78cbdd809a53d2d73a6c7c44dfa
oguzdurak16-create <oguzdurak16@gmail.com>
2026-07-23 08:25:59 +0300

    Fix duplicate consent analytics loading

--- components/AnalyticsBootstrap.jsx
+++ components/AnalyticsBootstrap.jsx
@@ -2,9 +2,25 @@
 
 import { useEffect, useRef } from 'react';
 import { usePathname } from 'next/navigation';
-import Script from 'next/script';
 
 const GTM_ID = 'GTM-5N62TLPD';
+const CONSENT_KEY = 'odyomuh-cookie-consent';
+const GTM_SELECTOR = `script[src*="googletagmanager.com/gtm.js?id=${GTM_ID}"]`;
+
+function sendPageView(pathname) {
+  if (window.localStorage.getItem(CONSENT_KEY) !== 'accepted') return false;
+  if (!window.google_tag_manager?.[GTM_ID]) return false;
+
+  const pagePath = `${pathname}${window.location.search || ''}`;
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function gtag() { window.dataLayer.push(arguments); };
+  window.gtag('event', 'page_view', {
+    page_title: document.title,
+    page_location: window.location.href,
+    page_path: pagePath,
+  });
+  return true;
+}
 
 export default function AnalyticsBootstrap() {
   const pathname = usePathname();
@@ -13,31 +29,21 @@ export default function AnalyticsBootstrap() {
   useEffect(() => {
     if (firstRender.current) {
       firstRender.current = false;
-      return;
+      return undefined;
     }
 
-    const pagePath = `${pathname}${window.location.search || ''}`;
+    if (sendPageView(pathname)) return undefined;
 
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = window.gtag || function gtag() { window.dataLayer.push(arguments); };
-    window.gtag('event', 'page_view', {
-      page_title: document.title,
-      page_location: window.location.href,
-      page_path: pagePath,
-    });
+    const gtmScript = document.querySelector(GTM_SELECTOR);
+    const onLoad = () => sendPageView(pathname);
+    gtmScript?.addEventListener('load', onLoad, { once: true });
+
+    const fallbackTimer = window.setTimeout(onLoad, 1500);
+    return () => {
+      window.clearTimeout(fallbackTimer);
+      gtmScript?.removeEventListener('load', onLoad);
+    };
   }, [pathname]);
 
-  return (
-    <Script id="gtm-loader" strategy="afterInteractive">{`
-      (function(w,d,s,l,i){
-        if(w.__ODYOMUH_GTM_LOADED__) return;
-        w.__ODYOMUH_GTM_LOADED__=true;
-        w[l]=w[l]||[];
-        w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-        f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${GTM_ID}');
-    `}</Script>
-  );
+  return null;
 }
