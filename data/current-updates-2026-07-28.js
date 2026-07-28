49031a20bb5a1f9d23015d0b2063ac92b2587971
oguzdurak16-create <oguzdurak16@gmail.com>
2026-07-28 08:07:22 +0300

    Daily content 2026-07-28

---
+++ data/current-updates-2026-07-28.js
@@ -0,0 +1,4 @@
+import { dailyTurkishPosts as previousTurkishPosts, dailyEnglishPosts as previousEnglishPosts } from './current-updates-2026-07-27.js';
+
+export const dailyTurkishPosts = [...previousTurkishPosts];
+export const dailyEnglishPosts = [...previousEnglishPosts];
