b79e7c80b53cb67140a8f9ea0a29dabc99c953db
oguzdurak16-create <oguzdurak16@gmail.com>
2026-08-09 08:05:45 +0300

    Daily content 2026-08-09

--- data/current-updates.js
+++ data/current-updates.js
@@ -1,4 +1,4 @@
-import { dailyTurkishPosts, dailyEnglishPosts } from './current-updates-2026-08-08.js';
+import { dailyTurkishPosts, dailyEnglishPosts } from './current-updates-2026-08-09.js';
 
 export const currentTurkishPosts = dailyTurkishPosts;
 export const currentEnglishPosts = dailyEnglishPosts;
@@ -12,4 +12,4 @@ function newestDate(items = []) {
 
 export const currentUpdateDate = newestDate([...currentTurkishPosts, ...currentEnglishPosts]);
 
-// Daily content verified: 2026-08-08
+// Daily content verified: 2026-08-09
