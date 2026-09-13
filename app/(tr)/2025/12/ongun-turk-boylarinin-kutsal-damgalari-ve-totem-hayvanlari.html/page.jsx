7edac97d21f50febf2893f6e5f44de40167778bc
oguzdurak16-create <oguzdurak16@gmail.com>
2026-09-14 00:23:47 +0300

    improve Ongun CTR metadata

---
+++ app/(tr)/2025/12/ongun-turk-boylarinin-kutsal-damgalari-ve-totem-hayvanlari.html/page.jsx
@@ -0,0 +1,41 @@
+import ContentPage from '../../../[...slug]/page';
+
+const slug = [
+  '2025',
+  '12',
+  'ongun-turk-boylarinin-kutsal-damgalari-ve-totem-hayvanlari.html',
+];
+const canonical = '/2025/12/ongun-turk-boylarinin-kutsal-damgalari-ve-totem-hayvanlari.html';
+const title = 'Ongun Nedir? Türk Boylarının Kutsal Hayvanları ve Damgaları';
+const description = 'Ongun nedir? Eski Türk boylarında kutsal kabul edilen hayvanları, boy damgalarıyla ilişkisini ve totem kavramından farkını kısa örneklerle öğrenin.';
+
+export const metadata = {
+  title,
+  description,
+  keywords: [
+    'ongun nedir',
+    'ongun nedir tarih',
+    'türklerde ongun ne demek',
+    'türk boylarının kutsal hayvanları',
+    'türk boyları damgaları',
+    'ongun totem farkı',
+  ],
+  alternates: { canonical },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-snippet': -1,
+      'max-image-preview': 'large',
+      'max-video-preview': -1,
+    },
+  },
+  openGraph: { type: 'article', title, description, url: canonical },
+  twitter: { card: 'summary_large_image', title, description },
+};
+
+export default function Page() {
+  return ContentPage({ params: { slug } });
+}
